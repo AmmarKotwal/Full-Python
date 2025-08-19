894ee2ed0291eb790802e3de8d7e2b6ec3360c91
Ammar <ammarthehakkiller@gmail.com>
2025-08-19 12:59:15 +0500

    admin panel

--- DjangoTemp/my_project/my_app/models.py
+++ DjangoTemp/my_project/my_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class emp(models.Model):
+    name = models.CharField(max_length = 50)
+    salary = models.IntegerField()
+    designation = models.CharField(max_length = 50)
+    department = models.CharField(max_length = 50)
+    qualification = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
